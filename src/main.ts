f376d95e0804ca0612ea296e3d83b17b7b91f83d
Ryuz <ryuzer@proton.me>
2026-02-24 08:58:40 +0100

    feat(errors): handle katex errors

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,66 @@
-import './style.css'
-import 'katex/dist/katex.min.css'
-import mermaid from 'mermaid'
-import katex from 'katex';
-import { z } from 'zod'
-import queryString from 'query-string';
+import "./style.css";
+import "katex/dist/katex.min.css";
+import mermaid from "mermaid";
+import katex from "katex";
+import { z } from "zod";
+import queryString from "query-string";
+
+function renderError(error: string) {
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <div style="white-space: pre;">
+      ${error}
+    </div>
+  `;
+}
+
+function renderJsonError(error: string) {
+  const prettyError = JSON.stringify(JSON.parse(error), undefined, "\t");
+  renderError(prettyError);
+}
 
 const QueryParamsSchema = z.object({
   method: z.enum(["renderMermaid", "renderKatex"]),
-  params: z.array(z.string())
-})
+  params: z.array(z.string()),
+});
 
-let searchParams: z.infer<typeof QueryParamsSchema> | undefined = undefined
+let searchParams: z.infer<typeof QueryParamsSchema> | undefined = undefined;
 
 try {
-  searchParams = QueryParamsSchema.parse(queryString.parse(window.location.search, {arrayFormat: 'bracket'}))
+  searchParams = QueryParamsSchema.parse(
+    queryString.parse(window.location.search, { arrayFormat: "bracket" }),
+  );
 } catch (error) {
   if (error instanceof z.ZodError) {
-    renderError(error.message)
+    renderJsonError(error.message);
   }
 }
 
-async function renderError(error: string) {
-  const prettyError = JSON.stringify(JSON.parse(error), undefined, '\t')
-  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <div style="white-space: pre;">
-      ${prettyError}
-    </div>
-  `
-}
-
 async function renderMermaid(graph: string) {
-  mermaid.initialize({})
-  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  mermaid.initialize({});
+  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <pre class="mermaid">
       ${graph}
     </pre>
-  `
-  await mermaid.run()
+  `;
+  await mermaid.run();
 }
 
 async function renderKatex(expression: string) {
-  const html = katex.renderToString(expression)
-  document.querySelector<HTMLDivElement>('#app')!.innerHTML = html
+  try {
+    const html = katex.renderToString(expression);
+    document.querySelector<HTMLDivElement>("#app")!.innerHTML = html;
+  } catch (error) {
+    if (error instanceof Error) {
+      renderError(error.message);
+    }
+  }
 }
 
 switch (searchParams?.method) {
   case "renderMermaid":
-    renderMermaid(atob(searchParams.params[0]))
-    break
+    renderMermaid(atob(searchParams.params[0]));
+    break;
   case "renderKatex":
-    renderKatex(atob(searchParams.params[0]))
-    break
+    renderKatex(atob(searchParams.params[0]));
+    break;
 }
